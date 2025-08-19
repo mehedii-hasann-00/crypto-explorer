df2ed3f97a72dfa2f4ec97465b80a7fe7bba2cb9
Mehedi Hasan <mehedihasan624358@gmail.com>
2025-08-19 19:31:19 +0600

    live data fetch add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,39 @@
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import Navbar from "./components/Navbar.js"
 function App() {
   const [coins, setCoins] = useState([]);
-  const [showMore, setShowMore] = useState(10);
+  const [oldData, setOldData] = useState([]);
+  const [coinLogos, setCoinLogos] = useState([]);
+
   useEffect(() => {
-    get_coins();
-    // setInterval(price_fetch, 3000);
-  }, [])
+    get_coins()
+  }, []);
 
-
-  const get_coins = async () => {
+  const get_coins = async()=>{
     let response = await fetch('http://localhost:5000/get');
     response = await response.json();
     setCoins(response.all_coins);
+    setCoinLogos(response.logos);
+    setTimeout(()=>fetch_latest(response.all_coins), 5000);
   }
-
-  // const price_fetch = ()=>{
-
-  //   setInterval(price_fetch, 3000);
-  // }
-
-  // console.log(coins[0].quote.USD.price)
+  const fetch_latest = async(prevCoins)=>{
+    
+    console.log('fetching...');
+    setOldData(prevCoins);
+    let response = await fetch('http://localhost:5000/fetch-latest');
+    response = await response.json();
+    
+    setCoins(response.all_coins);
+    setTimeout(()=>fetch_latest(response.all_coins), 5000);
+  }
+  console.log(oldData)
   return (
     <div className="mt-2 mx-2 md:mx-4 md:mt-6 lg:mt-8 lg:mx-12 ">
       <Navbar />
       <div className="text-center my-2 lg:my-12">
         <h1 className="relative text-2xl md:text-4xl lg:text-8xl font-bold bg-gradient-to-br from-orange-500 to-white-900 bg-clip-text text-transparent">
-          <span className="text-orange-500">{coins && coins[0] ? <img src={`${coins[0].logo}`} className="h-32 w-32 absolute left-[16%]"/> : "B"}</span>uy Real Money <br />
+          <span className="text-orange-500">{coins && coins[0] && coinLogos ? <img src={`${coinLogos[coins[0].id]}`} className="h-32 w-32 absolute left-[16%]"/> : "B"}</span>uy Real Money <br />
           <span className="text-2xl lg:text-6xl">with fake money</span>
         </h1>
       </div>
@@ -37,6 +43,9 @@ function App() {
             <tr>
               <th className="border border-gray-600 px-4 py-2">Rank</th>
               <th className="border border-gray-600 px-4 py-2">Name</th>
+              <th className="border border-gray-600 px-4 py-2">1h %</th>
+              <th className="border border-gray-600 px-4 py-2">24h %</th>
+              <th className="border border-gray-600 px-4 py-2">7d %</th>
               <th className="border border-gray-600 px-4 py-2">Price (USD)</th>
             </tr>
           </thead>
@@ -50,13 +59,24 @@ function App() {
                       className="group hover:bg-gray-700 hover:shadow-lg transition-all duration-200"
                     >
                       <td className="border border-gray-600 px-4 py-2 text-white group-hover:text-orange-400">
-                        {coin.cmc_rank}
+                        {coin.rank}
+                      </td>
+                      <td className="border border-gray-600 px-4 py-2 text-white group-hover:text-orange-400 relative">
+                        {coinLogos && coinLogos[coin.id] ?
+                        <img src={`${coinLogos[coin.id]}`} className="h-6 w-6 absolute left-[25%]"/> : null}
+                        <span>{coin.name}</span>
+                      </td>
+                      <td className={`border border-gray-600 px-4 py-2 ${oldData.length==0 ? "text-white" : coin.one_h > oldData[index].one_h ? "text-green-500" : "text-red-500"} group-hover:text-orange-400`}>
+                        {coin.one_h}
                       </td>
                       <td className="border border-gray-600 px-4 py-2 text-white group-hover:text-orange-400">
-                        {coin.name}
+                        {coin.one_day}
                       </td>
                       <td className="border border-gray-600 px-4 py-2 text-white group-hover:text-orange-400">
-                        {coin.quote?.USD?.price?.toFixed(2)}
+                        {coin.seven_day}
+                      </td>
+                      <td className={`border border-gray-600 px-4 py-2 ${oldData.length===0 ? "text-white" : coin.price > oldData[index].price ? "text-green-500" : "text-red-500"} group-hover:text-orange-400`}>
+                        {coin.price}
                       </td>
                     </tr>
                   );
--- server.js
+++ server.js
@@ -8,9 +8,39 @@ app.use(cors());
 const port = 5000;
 
 
-app.get('/fetch-price', (req, res) => {
-    
-    res.send('helo');
+app.get('/fetch-latest', async(req, res) => {
+    const api_string = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+    const response = await axios.get(
+        api_string,
+        {
+            headers: {
+                "X-CMC_PRO_API_KEY": "25f6c6b0-fa4b-4f58-915d-7fddd75f33ef",
+            }
+        }
+    );
+    let coins = response.data.data;
+
+    for (let i = 0; i < coins.length; i++) {
+        let temp = {
+            rank :coins[i].cmc_rank,
+            id: coins[i].id,
+            name: coins[i].name
+        };
+        if (coins[i].quote?.USD?.price) {
+            temp.price = coins[i].quote?.USD?.price?.toFixed(2);
+        }
+        if (coins[i].quote?.USD?.percent_change_1h) {
+            temp.one_h = coins[i].quote?.USD?.percent_change_1h.toFixed(2);
+        }
+        if (coins[i].quote?.USD?.percent_change_24h) {
+            temp.one_day = coins[i].quote?.USD?.percent_change_24h.toFixed(2);
+        }
+        if (coins[i].quote?.USD?.percent_change_7d) {
+            temp.seven_day = coins[i].quote?.USD?.percent_change_7d.toFixed(2);
+        }
+        coins[i] = temp;
+    }
+    res.send({ "all_coins": coins});
 })
 
 app.get("/get", async (req, res) => {
@@ -25,13 +55,16 @@ app.get("/get", async (req, res) => {
     );
     let coins = response.data.data;
     let coin_id = "";
+    let coin_logos = [];
     for (let i = 0; i < coins.length; i++) {
-        coin_id += coins[i].id;
-        if (i < coins.length - 1) {
-            coin_id += ",";
-        }
         if (i == 100) {
             break;
+        }
+        if (coins[i].id) {
+            coin_id += coins[i].id;
+            if (i < coins.length - 1) {
+                coin_id += ",";
+            }
         }
     }
 
@@ -48,12 +81,30 @@ app.get("/get", async (req, res) => {
         }
     );
     for (let i = 0; i < coins.length; i++) {
-        if ( coin_info.data.data[coins[i].id]) {
-            coins[i].logo = coin_info.data.data[coins[i].id].logo;
+        let temp = {
+            rank :coins[i].cmc_rank,
+            id: coins[i].id,
+            name: coins[i].name
+        };
+        if (coins[i].quote?.USD?.price) {
+            temp.price = coins[i].quote?.USD?.price?.toFixed(2);
         }
+        if (coins[i].quote?.USD?.percent_change_1h) {
+            temp.one_h = coins[i].quote?.USD?.percent_change_1h.toFixed(2);
+        }
+        if (coins[i].quote?.USD?.percent_change_24h) {
+            temp.one_day = coins[i].quote?.USD?.percent_change_24h.toFixed(2);
+        }
+        if (coins[i].quote?.USD?.percent_change_7d) {
+            temp.seven_day = coins[i].quote?.USD?.percent_change_7d.toFixed(2);
+        }
+        if (coin_info.data.data[coins[i].id]) {
+            coin_logos[coins[i].id] = coin_info.data.data[coins[i].id].logo;
+        }
+        coins[i] = temp;
     }
     console.log(coins[0]);
-    res.send({ "all_coins": coins });
+    res.send({ "all_coins": coins,"logos":coin_logos});
 
 });
 
